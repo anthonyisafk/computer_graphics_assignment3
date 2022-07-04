33340146b0cdb1f1c0d811176f3c8c1684916cb2
anthonyisafk <72876359+anthonyisafk@users.noreply.github.com>
2022-07-04 23:46:25 +0300

    Use helpers.normalize() to avoid division by zero in lighting.py

--- lighting.py
+++ lighting.py
@@ -11,7 +11,7 @@ def diffuse_light(P, N, color, kd, light_positions, light_intensities):
 
     for i in range(n):
         L = light_positions[i] - P
-        L_hat = L / np.linalg.norm(L)
+        L_hat = normalize(L)
         cos_a = np.dot(N, L_hat)
         cos_a = cos_a if cos_a > 0 else 0 
         I[i] = kd * cos_a * light_intensities[i]
@@ -22,11 +22,11 @@ def specular_light(P, N, color, cam_pos, ks, n, light_positions, light_intensiti
     s = len(light_positions)
     I = np.zeros((s, 3))
     V = cam_pos - P
-    V_hat = V / np.linalg.norm(V)
+    V_hat = normalize(V)
 
     for i in range(s):
         L = light_positions[i] - P
-        L_hat = L / np.linalg.norm(L)
+        L_hat = normalize(L)
         cos_a = np.dot(N, L_hat)
         cos_ba = abs(np.dot(2 * N * np.dot(N, L_hat) - L_hat, V_hat))
         if cos_ba <= 0 or cos_a <= 0:
