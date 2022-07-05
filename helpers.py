bc1a63bef55a3a9a7f2c36466de8fbec89cff74f
anthonyisafk <72876359+anthonyisafk@users.noreply.github.com>
2022-07-05 03:27:57 +0300

    Add image and vector color initialization, barycenter detection to helpers.py

--- helpers.py
+++ helpers.py
@@ -1,4 +1,20 @@
 import numpy as np
+import cv2 as cv
+
+
+def initialize_img(M, N, bg_color):
+    img = np.full((M, N, 3), bg_color)
+    return img
+
+
+def color_vertices(img, vert_colors, verts_rast, face_indices):
+    num_faces = len(face_indices)
+    for f in range(num_faces):
+        current_face = face_indices[f]
+        for i in range(len(current_face)):
+            vert = verts_rast[current_face[i]]
+            img[vert[0]][vert[1]] = vert_colors[current_face[i]]
+    return img
 
 
 def normalize(vec):
@@ -18,3 +34,8 @@ def get_keep_indices(face_indices, verts_rast, M, N):
         if positive_indices and in_bounds:
             keep_faces.append(current_face)
     return np.array(keep_faces)
+
+
+def find_barycenter(verts):
+    """Find the barycenter OF A TRIANGLE, given its vertex coordinates."""
+    return np.sum(verts, axis=0) / 3
