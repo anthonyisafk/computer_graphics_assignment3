62df0141cffabb350deb535364c967376af45b9d
anthonyisafk <72876359+anthonyisafk@users.noreply.github.com>
2022-07-08 01:20:46 +0300

    Start working on the report

---
+++ report/report.jl
@@ -0,0 +1,296 @@
+### A Pluto.jl notebook ###
+# v0.17.4
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 3974ffe0-fd76-11ec-3dc0-ed1212b699c2
+begin
+	using Markdown
+	using InteractiveUtils
+	using Markdown
+	using InteractiveUtils
+	using PlutoUI
+end
+
+# ╔═╡ 320d11c4-745b-4471-b696-fac62305a3ef
+md"""
+# Γραφική με Υπολογιστές
+## Αριστοτέλειο Πανεπιστήμιο Θεσσαλονίκης - Τμήμα Ηλεκτρολόγων Μηχανικών Μηχανικών Υπολογιστών
+### 3η Εργασία Εξαμήνου: Θέαση 
+### Αντωνίου Αντώνιος - aantonii@ece.auth.gr - 9482 
+"""
+
+# ╔═╡ 7568237d-3d40-4bf0-a697-096fd3b90baf
+md"""
+## Σκοπός της εργασίας
+Στο τρίτο και τελευταίο μέρος των εργασίων του μαθήματος, αναλαμβάνουμε τη σκίαση και τη θέαση ενός τρισδιάστατου αντικειμένου χρησιμοποιώντας δύο διαφορετικούς αλγορίθμους, με τρία διαφορετικά είδη φωτισμού.
+\
+Τα αρχικά δεδομένα που δίνονται είναι οι συντελεστές ανάκλασης διάχυτου φωτός, διάχυτης και κατοπτρικής ανάκλασης, καθώς και η διάχυτη ακτινοβολία του χώρου, μαζί με την ακτινοβολία και την θέση όποιων άλλων φωτεινών πηγών. Αυτά τα δεδομένα βρίσκονται σε συνδυασμό με τις θέσεις (στο WCS) των κορυφών των τριγώνων από τα οποία απαρτίζεται το αντικείμενο και, φυσικά, τις παραμέτρους της κάμερας (όπως στη 2η εργασία).
+\
+Παρακάτω αναλύεται η ανάπτυξη των συναρτήσεων που ζητούνται από την εκφώνηση της εργασίας, καθώς και οι βασικότερες εκ των επιπλέον συναρτήσεων που χρειάστηκαν για την ολοκλήρωση του στόχου.
+
+## ambient_light
+Δεδομένης διάχυτης ακτινοβολίας $I_{a}$ και συντελεστή ανάκλασης διάχυτου φωτός $k_{a}$, επιστρέφει το φωτισμό ενός (άρα και όλων των σημείων ενός στερεού με σταθερό $k_{a}$, σαν αυτό που πραγματευόμαστε).
+
+$I_{a}=k_{a}\cdot I_{a}$
+
+## diffuse_light
+Για σημείο **P** κάθε μία από τις θέσεις και τις εντάσεις του φωτός (entries στις `light_positions` και `light_intensities`), υπολογίζουμε τη διάχυτη ανάκλαση, βάσει της θεωρίας:
+
+Έστω $\mathscr{S}$ η θέση και $\mathscr{I}$ ο 3x1 πίνακας της έντασης του φωτός
+* $\overline{L}=\mathscr{S}-P$
+* $\hat{L}=\frac{\overline{L}}{||L||}$
+* $cos(\alpha)=(\hat{N},\hat{L})$
+Όπου $\alpha$ είναι η γωνία μεταξύ του μοναδιαίου $\hat{L}$ και του μοναδιαίου $\hat{N}$, που είναι γνωστό και παριστά το **normal vector** της επιφάνειας της οποίας το P είναι μέρος.
+\
+Τώρα, αν, εν τέλει, βρεθεί πως $cos(\alpha)>0$, υπολογίζουμε:
+
+$I=k_{d}\cdot cos(\alpha)\cdot \mathscr{I}$
+Στό τέλος, όλοι αυτοί οι πίνακες I για κάθε πηγή φωτός προστίθενται και το αποτέλεσμα πολλαπλασιάζεται με το **color** που δίνεται στη συνάρτηση για το σημείο P.
+
+## specular_light
+Η συνάρτηση υπολογισμού φωτισμού από κατοπτρική ανάκλαση, δέχεται τα ίδια ορίσματα με την `diffuse_light`, με την προσθήκη της θέσης του παρατηρητή (δηλαδή της κάμερας, έστω $\mathscr{C}$) και του **συντελεστή Phong** $\mathscr{n}$.
+\
+Ξανά, για κάθε entry στη λίστα των θέσεων και των εντάσεων των φωτεινών πηγών, κι αφού έχουμε εξάγει τα $\hat{L}$ και $cos(\alpha)$, ακριβώς όπως παραπάνω, έχουμε:
+
+* $\overline{V}=\mathscr{C}-P$
+* $\hat{V}=\frac{\overline{V}}{||V||}$
+* $cos(\beta-\alpha)=(2\cdot \hat{N}\cdot cos(\alpha)-\hat{L})\cdot \hat{V}$
+* $I=k_s\cdot\ cos(\beta-\alpha)^{\mathscr{n}}\cdot\mathscr{I}$
+Όπως και πριν, το άθροισμα των παραγόμενων I πολλαπλασιάζεται με το δοθέν **color** και επιστρέφεται.
+
+## calculate_normals
+Με δεδομένες τις *WCS* συντεταγμένες των κορυφών των τριγώνων και του πίνακα **face_indices**, που περιγράφει από ποιες κορυφές αποτελείται το καθένα, θέλουμε να υπολογίσουμε τα **normal vectors** σε κάθε κορυφή.
+\
+Γι' αυτό το σκοπό, για κάθε τρίγωνο, βρίσκουμε το normal vector του και το προσθέτουμε στα αντίστοιχα index των κορυφών του στον πίνακα *normals*, όπου αποθηκευέται το αποτέλεσμα που θα επιστραφεί. Αν $A, B, C$ είναι οι κορυφές ενός τυχαίου τριγώνου, τότε για το normal vector του:
+
+* $\overline{V}_{1}=B-A$
+* $\overline{V}_{2}=C-A$
+* $\overline{N}=\overline{V}_{1}\times\overline{V}_{2}$
+Στο τέλος όλων των επαναλήψεων (όταν δηλαδή έχουμε υπολογίσει το κάθετο διάνυσμα κάθε τριγώνου), έχουμε τη συνεισφορά του κάθε τριγώνου σε όσους κορυφές το αποτελούν. Επιστρέφουμε τα κανονικοποιημένα διανύσματα εντός του *normals*.
+
+## render_object
+Είναι η κεντρική συνάρτηση του κώδικα. Μέσα σε αυτήν αρχικοποιείται η εικόνα ως πίνακας **numpy array (MxNx3)**, υπολογίζονται τα **κάθετα διανύσματα**, γίνεται **προβολή** στον κόσμο της κάμερας (άρα και **υπολογισμός των depths**) και **rasterize** στις τρισδιάστατες κορυφές και, τέλος κρατάμε μόνο τα τρίγωνα τα οποία έχουν _**όλες τους τις κορυφές εντός των ορίων της εικόνας**_.
+\
+Για αυτά τα τρίγωνα, βρίσκουμε τα **βαρύκεντρα**, των οποίων οι συντεταγμένες στο WCS είναι απλά *ο μέσος όρος των ομόλογων συντεταγμένων των κορυφών*.
+\
+Τώρα, για κάθε ένα τρίγωνο ξεχωριστά, με σειρά που καθορίζεται από τα βάθη τους, με τα πιο απομακρυσμένα να περνάνε πρώτα από επεξεργασία, χρωματίζουμε και σκιάζουμε την περιοχή που καταλαμβάνει. Το πώς, καθορίζεται από τη μετάβλητή `shader`, που παίρνει τις τιμές *"gouraud"* ή *"phong"*, ούτως ώστε να κληθεί η αντίστοιχη συνάρτηση.
+
+## 
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+InteractiveUtils = "b77e0a4c-d291-57a0-90e8-8db25a27a240"
+Markdown = "d6f4376e-aef5-505a-96c1-9c027394607a"
+PlutoUI = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
+
+[compat]
+PlutoUI = "~0.7.39"
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+[[AbstractPlutoDingetjes]]
+deps = ["Pkg"]
+git-tree-sha1 = "8eaf9f1b4921132a4cff3f36a1d9ba923b14a481"
+uuid = "6e696c72-6542-2067-7265-42206c756150"
+version = "1.1.4"
+
+[[ArgTools]]
+uuid = "0dad84c5-d112-42e6-8d28-ef12dabb789f"
+
+[[Artifacts]]
+uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
+
+[[Base64]]
+uuid = "2a0f44e3-6c83-55bd-87e4-b1978d98bd5f"
+
+[[ColorTypes]]
+deps = ["FixedPointNumbers", "Random"]
+git-tree-sha1 = "eb7f0f8307f71fac7c606984ea5fb2817275d6e4"
+uuid = "3da002f7-5984-5a60-b8a6-cbb66c0b333f"
+version = "0.11.4"
+
+[[Dates]]
+deps = ["Printf"]
+uuid = "ade2ca70-3891-5945-98fb-dc099432e06a"
+
+[[Downloads]]
+deps = ["ArgTools", "LibCURL", "NetworkOptions"]
+uuid = "f43a241f-c20a-4ad4-852c-f6b1247861c6"
+
+[[FixedPointNumbers]]
+deps = ["Statistics"]
+git-tree-sha1 = "335bfdceacc84c5cdf16aadc768aa5ddfc5383cc"
+uuid = "53c48c17-4a7d-5ca2-90c5-79b7896eea93"
+version = "0.8.4"
+
+[[Hyperscript]]
+deps = ["Test"]
+git-tree-sha1 = "8d511d5b81240fc8e6802386302675bdf47737b9"
+uuid = "47d2ed2b-36de-50cf-bf87-49c2cf4b8b91"
+version = "0.0.4"
+
+[[HypertextLiteral]]
+deps = ["Tricks"]
+git-tree-sha1 = "c47c5fa4c5308f27ccaac35504858d8914e102f9"
+uuid = "ac1192a8-f4b3-4bfe-ba22-af5b92cd3ab2"
+version = "0.9.4"
+
+[[IOCapture]]
+deps = ["Logging", "Random"]
+git-tree-sha1 = "f7be53659ab06ddc986428d3a9dcc95f6fa6705a"
+uuid = "b5f81e59-6552-4d32-b1f0-c071b021bf89"
+version = "0.2.2"
+
+[[InteractiveUtils]]
+deps = ["Markdown"]
+uuid = "b77e0a4c-d291-57a0-90e8-8db25a27a240"
+
+[[JSON]]
+deps = ["Dates", "Mmap", "Parsers", "Unicode"]
+git-tree-sha1 = "3c837543ddb02250ef42f4738347454f95079d4e"
+uuid = "682c06a0-de6a-54ab-a142-c8b1cf79cde6"
+version = "0.21.3"
+
+[[LibCURL]]
+deps = ["LibCURL_jll", "MozillaCACerts_jll"]
+uuid = "b27032c2-a3e7-50c8-80cd-2d36dbcbfd21"
+
+[[LibCURL_jll]]
+deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll", "Zlib_jll", "nghttp2_jll"]
+uuid = "deac9b47-8bc7-5906-a0fe-35ac56dc84c0"
+
+[[LibGit2]]
+deps = ["Base64", "NetworkOptions", "Printf", "SHA"]
+uuid = "76f85450-5226-5b5a-8eaa-529ad045b433"
+
+[[LibSSH2_jll]]
+deps = ["Artifacts", "Libdl", "MbedTLS_jll"]
+uuid = "29816b5a-b9ab-546f-933c-edad1886dfa8"
+
+[[Libdl]]
+uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
+
+[[LinearAlgebra]]
+deps = ["Libdl"]
+uuid = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
+
+[[Logging]]
+uuid = "56ddb016-857b-54e1-b83d-db4d58db5568"
+
+[[Markdown]]
+deps = ["Base64"]
+uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
+
+[[MbedTLS_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "c8ffd9c3-330d-5841-b78e-0817d7145fa1"
+
+[[Mmap]]
+uuid = "a63ad114-7e13-5084-954f-fe012c677804"
+
+[[MozillaCACerts_jll]]
+uuid = "14a3606d-f60d-562e-9121-12d972cd8159"
+
+[[NetworkOptions]]
+uuid = "ca575930-c2e3-43a9-ace4-1e988b2c1908"
+
+[[Parsers]]
+deps = ["Dates"]
+git-tree-sha1 = "0044b23da09b5608b4ecacb4e5e6c6332f833a7e"
+uuid = "69de0a69-1ddd-5017-9359-2bf0b02dc9f0"
+version = "2.3.2"
+
+[[Pkg]]
+deps = ["Artifacts", "Dates", "Downloads", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "REPL", "Random", "SHA", "Serialization", "TOML", "Tar", "UUIDs", "p7zip_jll"]
+uuid = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
+
+[[PlutoUI]]
+deps = ["AbstractPlutoDingetjes", "Base64", "ColorTypes", "Dates", "Hyperscript", "HypertextLiteral", "IOCapture", "InteractiveUtils", "JSON", "Logging", "Markdown", "Random", "Reexport", "UUIDs"]
+git-tree-sha1 = "8d1f54886b9037091edf146b517989fc4a09efec"
+uuid = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
+version = "0.7.39"
+
+[[Printf]]
+deps = ["Unicode"]
+uuid = "de0858da-6303-5e67-8744-51eddeeeb8d7"
+
+[[REPL]]
+deps = ["InteractiveUtils", "Markdown", "Sockets", "Unicode"]
+uuid = "3fa0cd96-eef1-5676-8a61-b3b8758bbffb"
+
+[[Random]]
+deps = ["Serialization"]
+uuid = "9a3f8284-a2c9-5f02-9a11-845980a1fd5c"
+
+[[Reexport]]
+git-tree-sha1 = "45e428421666073eab6f2da5c9d310d99bb12f9b"
+uuid = "189a3867-3050-52da-a836-e630ba90ab69"
+version = "1.2.2"
+
+[[SHA]]
+uuid = "ea8e919c-243c-51af-8825-aaa63cd721ce"
+
+[[Serialization]]
+uuid = "9e88b42a-f829-5b0c-bbe9-9e923198166b"
+
+[[Sockets]]
+uuid = "6462fe0b-24de-5631-8697-dd941f90decc"
+
+[[SparseArrays]]
+deps = ["LinearAlgebra", "Random"]
+uuid = "2f01184e-e22b-5df5-ae63-d93ebab69eaf"
+
+[[Statistics]]
+deps = ["LinearAlgebra", "SparseArrays"]
+uuid = "10745b16-79ce-11e8-11f9-7d13ad32a3b2"
+
+[[TOML]]
+deps = ["Dates"]
+uuid = "fa267f1f-6049-4f14-aa54-33bafae1ed76"
+
+[[Tar]]
+deps = ["ArgTools", "SHA"]
+uuid = "a4e569a6-e804-4fa4-b0f3-eef7a1d5b13e"
+
+[[Test]]
+deps = ["InteractiveUtils", "Logging", "Random", "Serialization"]
+uuid = "8dfed614-e22c-5e08-85e1-65c5234f0b40"
+
+[[Tricks]]
+git-tree-sha1 = "6bac775f2d42a611cdfcd1fb217ee719630c4175"
+uuid = "410a4b4d-49e4-4fbc-ab6d-cb71b17b3775"
+version = "0.1.6"
+
+[[UUIDs]]
+deps = ["Random", "SHA"]
+uuid = "cf7118a7-6976-5b1a-9a39-7adc72f591a4"
+
+[[Unicode]]
+uuid = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
+
+[[Zlib_jll]]
+deps = ["Libdl"]
+uuid = "83775a58-1f1d-513f-b197-d71354ab007a"
+
+[[nghttp2_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "8e850ede-7688-5339-a07c-302acd2aaf8d"
+
+[[p7zip_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
+"""
+
+# ╔═╡ Cell order:
+# ╟─3974ffe0-fd76-11ec-3dc0-ed1212b699c2
+# ╠═320d11c4-745b-4471-b696-fac62305a3ef
+# ╠═7568237d-3d40-4bf0-a697-096fd3b90baf
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
